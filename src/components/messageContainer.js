ad3f3a9b07fb26754ff609db7b182000b0b52e94
surya8020 <surya8020@gmail.com>
2023-08-13 19:40:35 -0700

    update message location

--- src/components/messageContainer.js
+++ src/components/messageContainer.js
@@ -1,7 +1,7 @@
 const MessageContainer = ({ messages, curUser }) => {
     return <div className='message-container'>
         {messages.map((m, index) =>
-            <div key={index} className={m.user === curUser ? 'my-message' : 'user-message'}>
+            <div key={index} className={m.user === curUser ? 'user-message' : 'my-message'}>
                 <div className={m.user === curUser ? 'message bg-success' : 'message bg-primary'}>{m.message}</div>
                 <div className='from=user'>{m.user}</div>
             </div>
